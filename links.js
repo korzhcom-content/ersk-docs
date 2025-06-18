655f48dc597635716453358a8f58ef01bbaec458
Oleksandr Melnychenko <green@crocodile.in.ua>
2025-06-18 19:02:13 +0300

    Update links

--- links.js
+++ links.js
@@ -8,10 +8,5 @@ export default [
         id: 2,
         label: "Demo",
         link: "https://easy.report",
-    },
-    {
-        id: 3,
-        label: "Sign-In",
-        link: "https://korzh.com/account",
-    },
+    }
 ]
